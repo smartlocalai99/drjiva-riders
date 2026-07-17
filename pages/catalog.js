a73bed0505cd2e9f66df6ff62ca05b4f38d87cf4
Vardhan reddy <vardhanreddy@Vardhans-MacBook-Air.local>
2026-07-18 00:38:13 +0530

    Remove unused eslint-disable comment left over from race-condition fix
    The exhaustive-deps rule wasn't actually firing here, so the disable
    directive was dead weight (flagged by ESLint's own
    "unused eslint-disable directive" check).

--- pages/catalog.js
+++ pages/catalog.js
@@ -28,7 +28,6 @@ export default function Catalog() {
 
   useEffect(() => {
     runSearch(query);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
 
   const handleAdd = async (e) => {
